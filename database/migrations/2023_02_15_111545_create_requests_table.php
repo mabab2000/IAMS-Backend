94eeefe2b914ad8fdb61f04ad7503425a265653e
mabab2000 <88932706+mabab2000@users.noreply.github.com>
2023-03-13 08:35:10 +0200

    Add files via upload

---
+++ database/migrations/2023_02_15_111545_create_requests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('depart_id')->default(1);
+            $table->foreign('depart_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->string('req_name');
+            $table->string('letter');
+            $table->string('recom_letter');
+            $table->string('description');
+            
+            $table->string('status')->default('Pending');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
